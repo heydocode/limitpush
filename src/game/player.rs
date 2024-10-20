d64684853d47efecc486d26b54ca69fc8f937c94
heydocode <169077757+heydocode@users.noreply.github.com>
2024-10-20 22:21:46 +0200

    Enhance performance
    Remove redundant Player component updates (you can use the Player entity Transform instead which is pre-implemented

--- src/game/player.rs
+++ src/game/player.rs
@@ -6,9 +6,10 @@ use crate::states::screens::{loading::PlayerAssets, Screen};
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Screen::Pipeline), spawn_player);
     app.add_systems(Update, apply_movement.run_if(in_state(Screen::Playing)));
+    app.register_type::<Player>();
 }
 
-#[derive(Component)]
+#[derive(Component, Reflect)]
 pub struct Player {
     pub health: f64,
     pub min_speed: f64,
@@ -23,14 +24,13 @@ fn spawn_player(mut commands: Commands, player_assets: Res<PlayerAssets>) {
             transform: Transform::from_xyz(0.0, 0.0, 0.0),
             ..default()
         },
+        Name::new("Player"),
         Player {
             health: 1.0,
             min_speed: 1.5,
             max_speed: 8.0,
             jump_intensity: 10.0,
         },
-        // RigidBody::Dynamic,
-        // Collider::capsule(2.0, 2.0),
     );
 
     commands.spawn(player);
@@ -56,6 +56,9 @@ fn apply_movement(
     }
     if input.pressed(KeyCode::Space) {
         direction.y += 1.;
+    }
+    if input.pressed(KeyCode::ShiftLeft) {
+        direction.y -= 1.;
     }
     transform.translation.x += direction.x * 1.;
     transform.translation.y += direction.y * 1.;
