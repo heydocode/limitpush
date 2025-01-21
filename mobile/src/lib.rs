996de9e0b71a24695aa1afb16b4b51e301c5fefd
heydocode <169077757+heydocode@users.noreply.github.com>
2025-01-21 20:12:59 +0100

    mobile update

--- mobile/src/lib.rs
+++ mobile/src/lib.rs
@@ -2,9 +2,9 @@ use bevy::{
     app::{App, AppExit},
     prelude::bevy_main,
 };
-use limitpush::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
+use limitpush::AssemblerPlugin; // BRANDING: Replace bevy_game with your new crate name.
 
 #[bevy_main]
 fn main() -> AppExit {
-    App::new().add_plugins(GamePlugin).run()
+    App::new().add_plugins(AssemblerPlugin).run()
 }
